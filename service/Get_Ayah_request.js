408de74855df94d342133f66681fb2c79afb228a
Mohmad-Naser-alnakeshbandi <mhmdn1381@gmail.com>
2023-08-17 20:55:45 +0200

    add some style

--- service/Get_Ayah_request.js
+++ service/Get_Ayah_request.js
@@ -1,45 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import axios from 'react-native-axios';
+import axios from 'react-native-axios'; // Import axios without the 'react-native-' prefix
 
-const Get_Ayah_request = () => {
-    const [surahname, setSurahName] = useState(null);
-    const [ayah, setAyah] = useState(null);
-    const [ayahnumber, setAyahNumber] = useState(null);
-    useEffect(() => {
-      // Define the URL of the API you want to request in able to get the link
-      const SurahsURL = 'https://cdn.jsdelivr.net/npm/quran-json@3.1.2/dist/chapters/index.json';
-      // Make the GET request using Axios
-      axios.get(SurahsURL)
-        .then(Surahresponse => {
-            var Surahmin= 0;
-            var Surahmax = Surahresponse.data.length;
-            const randomSurah = Math.floor(Math.random() * (Surahmax - Surahmin + 1)) + Surahmin;
-            console.log(Surahresponse.data[randomSurah].name)
-            setSurahName(Surahresponse.data[randomSurah].name)
-            axios.get(Surahresponse.data[randomSurah].link).then(Ayahresponse =>{
-                var Ayahmin= 0
-                var Ayahmax = Ayahresponse.data.verses.length
-                const randomAyah = Math.floor(Math.random() * (Ayahmax - Ayahmin + 1)) + Ayahmin;
-                console.log(Ayahresponse.data.verses[randomAyah].id)
-                setAyahNumber(Ayahresponse.data.verses[randomAyah].id)
-                setAyah(Ayahresponse.data.verses[randomAyah].text)
-            })
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
+const GetRandomAyah = () => {
+  const [surahName, setSurahName] = useState(null);
+  const [ayah, setAyah] = useState(null);
+  const [ayahNumber, setAyahNumber] = useState(null);
 
-    }, []); // Empty dependency array means this effect runs only once on component mount
+  useEffect(() => {
+    const fetchSurahs = async () => {
+      try {
+        const response = await axios.get(
+          'https://cdn.jsdelivr.net/npm/quran-json@3.1.2/dist/chapters/index.json'
+        );
+        const surahs = response.data;
+        const randomSurahIndex = Math.floor(Math.random() * surahs.length);
+        const randomSurah = surahs[randomSurahIndex];
 
+        console.log(randomSurah.name);
+        setSurahName(randomSurah.name);
+
+        const ayahResponse = await axios.get(randomSurah.link);
+        const ayahs = ayahResponse.data.verses;
+        const randomAyahIndex = Math.floor(Math.random() * ayahs.length);
+        const randomAyah = ayahs[randomAyahIndex];
+
+        console.log(randomAyah.id);
+        setAyahNumber(randomAyah.id);
+        setAyah(randomAyah.text);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchSurahs();
+  }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{ayah}</Text>
-      <Text>{surahname} {ayahnumber}</Text>
+    <View style={styles.container}>
+      <Text style={styles.ayahText}>{ayah}</Text>
+      <Text style={styles.surahAyahText}>
+        {surahName} {ayahNumber}
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default Get_Ayah_request
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: "10%",
+    paddingRight: "10%",
+  },
+  ayahText: {
+    fontSize: 18,
+    marginBottom: 25,
+  },
+  surahAyahText: {
+    fontSize: 18,
+  },
+});
 
+export default GetRandomAyah;
